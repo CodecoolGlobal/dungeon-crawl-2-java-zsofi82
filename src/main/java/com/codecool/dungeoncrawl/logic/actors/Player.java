b6468dec7f6461997fedc53d38867ab334747924
zsofi82 <zsofigaal@gmail.com>
2022-07-26 10:04:41 +0200

    Hero not able to move into walls and skeletons

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Player extends Actor {
     public Player(Cell cell) {
@@ -9,5 +10,18 @@ public class Player extends Actor {
 
     public String getTileName() {
         return "player";
+    }
+    Cell PlayerCell = getCell();
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = PlayerCell.getNeighbour(dx, dy);
+        System.out.println(nextCell.getType());
+        if (nextCell.getType().equals(CellType.FLOOR) && !(nextCell.getActor() instanceof Skeleton)) {
+            PlayerCell.setActor(null);
+            nextCell.setActor(this);
+            PlayerCell = nextCell;
+        } else {
+            PlayerCell.setActor(this);
+        }
     }
 }
