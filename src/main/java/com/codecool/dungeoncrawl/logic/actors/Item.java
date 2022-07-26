c2b7cb012ac0f334898f1bf47be518db576ef51d
footb <dominikvargra@gmail.com>
2022-07-26 14:31:27 +0200

    put item on the map

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Item.java
@@ -0,0 +1,14 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.Drawable;
+
+public abstract class Item implements Drawable {
+
+    Cell cell;
+
+    public Item(Cell cell) {
+        this.cell = cell;
+    }
+
+}
