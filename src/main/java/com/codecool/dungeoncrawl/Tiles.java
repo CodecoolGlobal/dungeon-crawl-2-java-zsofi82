6b006646516df037f1eba5a70e8f039f9004faf3
zsofi82 <zsofigaal@gmail.com>
2022-08-01 11:22:17 +0200

    Change the hero to green Figure

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -25,7 +25,7 @@ public class Tiles {
         tileMap.put("empty", new Tile(0, 0));
         tileMap.put("wall", new Tile(10, 17));
         tileMap.put("floor", new Tile(2, 0));
-        tileMap.put("player", new Tile(27, 0));
+        tileMap.put("player", new Tile(20, 9));
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("sword", new Tile(0, 30));
         tileMap.put("key", new Tile(16, 23));
