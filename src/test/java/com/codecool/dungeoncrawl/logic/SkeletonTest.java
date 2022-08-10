b6bd44fbf169ed67d9cf6878133fb087ed7192e4
zsofi82 <zsofigaal@gmail.com>
2022-08-10 16:26:22 +0200

    SkeletonTest: move if can

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/SkeletonTest.java
@@ -0,0 +1,30 @@
+package com.codecool.dungeoncrawl.logic;
+
+import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SkeletonTest {
+    GameMap gameMap;
+    Skeleton skeleton;
+
+    @BeforeEach
+    public void initGame() {
+        this.gameMap = new GameMap(5, 5, CellType.FLOOR);
+        this.skeleton = new Skeleton(gameMap.getCell(2, 2));
+    }
+
+    @Test
+    public void moveSkeletonIfCanMove() {
+        int starterX = skeleton.getX();
+        int starterY = skeleton.getY();
+        skeleton.act();
+
+        boolean expected = true;
+        boolean result = starterX != skeleton.getX() || starterY != skeleton.getY();
+
+        assertEquals(expected, result);
+    }
+}
