875b52a496c46550049beec8bbaae338988f42d1
csaszarattila42 <csaszarattila90@gmail.com>
2022-08-11 22:52:19 +0200

    feat: Format error popup a bit

--- src/main/java/com/codecool/dungeoncrawl/ui/ErrorPopup.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/ErrorPopup.java
@@ -1,5 +1,7 @@
 package com.codecool.dungeoncrawl.ui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -16,6 +18,9 @@ public class ErrorPopup extends MyPopup {
         OKButton.setOnAction(event -> window.close());
         messageLabel = new Label("");
 
+        mainContainer.setAlignment(Pos.CENTER);
+        mainContainer.setPadding(new Insets(10));
+        mainContainer.setSpacing(5);
         mainContainer.getChildren().add(messageLabel);
         mainContainer.getChildren().add(OKButton);
     }
