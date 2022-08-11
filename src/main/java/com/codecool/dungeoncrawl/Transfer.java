8048b5e284fb758c7d6e4f46743022f50e94c338
csaszarattila42 <csaszarattila90@gmail.com>
2022-08-12 00:30:42 +0200

    Attempt to import map

--- src/main/java/com/codecool/dungeoncrawl/Transfer.java
+++ src/main/java/com/codecool/dungeoncrawl/Transfer.java
@@ -1,13 +1,22 @@
 package com.codecool.dungeoncrawl;
 
 import com.codecool.dungeoncrawl.logic.GameMap;
-import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
+import com.codecool.dungeoncrawl.logic.actors.Actor;
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import com.codecool.dungeoncrawl.logic.actors.Zombie;
+import com.google.gson.*;
 
 import java.io.*;
+import java.lang.reflect.Type;
 
-public class Transfer {
-    Gson gson = new Gson();
+public class Transfer implements JsonDeserializer<Actor>, JsonSerializer<Actor> {
+    Gson gson;
+
+    public Transfer() {
+        GsonBuilder builder = new GsonBuilder();
+        gson = builder.registerTypeAdapter(Actor.class, this).create();
+    }
 
     public void toJson(GameMap map, String filename) throws JsonIOException, IOException {
         gson.toJson(map, new FileWriter(filename));
@@ -15,5 +24,30 @@ public class Transfer {
 
     public GameMap fromJson(String filename) throws FileNotFoundException {
         return gson.fromJson(new FileReader(filename), GameMap.class);
+    }
+
+    @Override
+    public JsonElement serialize(Actor actor, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject result = new JsonObject();
+        result.add("health", new JsonPrimitive(actor.getHealth()));
+        result.add("tilename", new JsonPrimitive(actor.getTileName()));
+        return result;
+    }
+
+    @Override
+    public Actor deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        JsonObject actor = jsonElement.getAsJsonObject();
+        switch (actor.get("tilename").getAsString()) {
+            case "player" -> {
+                return new Player(null);
+            }
+            case "skeleton" -> {
+                return new Skeleton(null);
+            }
+            case "zombie" -> {
+                return new Zombie(null);
+            }
+        }
+        return null;
     }
 }
